7cbc1ea9dc3299e0cc26a6162aab3488f2b7fa73
RWallan <3am.richardwallan@gmail.com>
2025-03-17 18:13:02 -0300

    feat: button component

---
+++ wren-garden/src/lib/components/Button.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  interface ComponentProps {
+    children: Snippet;
+    isSecondary?: boolean;
+    onclick?: (e: MouseEvent) => void;
+    type?: "submit" | "button";
+  }
+
+  let { children, isSecondary, onclick, ...props }: ComponentProps = $props();
+</script>
+
+{#if isSecondary}
+  <button
+    {...props}
+    {onclick}
+    class="min-w-15 text-gray p-3 cursor-pointer text-2xl font-bold"
+    >{@render children()}</button
+  >
+{:else}
+  <button
+    {...props}
+    class="min-w-60 px-3 py-2 cursor-pointer bg-primary rounded-2xl text-light-yellow text-2xl font-bold"
+    {onclick}
+  >
+    {@render children()}
+  </button>
+{/if}
